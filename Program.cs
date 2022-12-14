936569c7a73179ff9ff913958ff5afb8ffcb3362
Patricia Takehara <paty.takeharab@outlook.com>
2022-12-14 18:05:10 -0300

    Commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// Stack => Empilhar 
+//LIFO => last in first out - Ultimo que entrou vai ser o primeiro a sair
+
+Stack<int> pilha = new Stack<int>();
+
+pilha.Push(1);
+pilha.Push(3);
+pilha.Push(5);
+pilha.Push(7);
+pilha.Push(9);
+
+Console.WriteLine("Adicionando item na pilha:");
+
+foreach (int item in pilha)
+{
+  Console.WriteLine($"Item {item}");
+}
+
+Console.WriteLine();
+Console.WriteLine("Removendo item da pilha:");
+Console.WriteLine($"Item {pilha.Pop()}");
+
+foreach (int item in pilha)
+{
+  Console.WriteLine($"Item {item}");
+}
